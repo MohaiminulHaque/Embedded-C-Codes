30cf852ab589695c1dd6e0c084a05b761bbd36df
Md Mohaiminul Haque <113456220+MohaiminulHaque@users.noreply.github.com>
2024-02-14 01:31:57 +0200

    Add files via upload

---
+++ structure_pointer_function(call_by_reference).c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+struct data
+{
+	char Data1;
+	int Data2;
+	short Data3;
+}varData;
+
+void DisplayElements(struct data *pointerData)
+{
+	int Data4;
+	//WriteData
+	pointerData->Data1=0x22; //for writing, this arrow operator is used.
+	//ReadData
+	Data4= pointerData->Data1;
+	printf("stored value in data1: %x\n", varData.Data1);
+	printf("stored value in data1: %x\n", Data4);
+}
+
+int main ()
+
+{
+	varData.Data1=0x11;
+	varData.Data2=0xaaaaffff;
+	varData.Data3=0xabcd;
+	printf("stored value in data1: %x\n", varData.Data1);
+	DisplayElements(&varData);
+
+
+	return 0;
+}
